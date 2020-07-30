66d7d38351936566342d7111f81e352e5ed995ff
JanWielgus <jan.wielgus12@gmail.com>
2020-07-30 17:05:48 +0200

    Implementation of AverageFilter

---
+++ AverageFilter.h
@@ -0,0 +1,75 @@
+/**
+ * @file AverageFilter.h
+ * @author your name (you@domain.com)
+ * @brief Average filter library
+ * @date 2020-07-30
+ * 
+ */
+
+#ifndef AVERAGEFILTER_H
+#define AVERAGEFILTER_H
+
+#include <IFilter.h>
+
+
+template <class T, class SumType>
+class AverageFilter : public IFilter<T>
+{
+private:
+    const uint16_t SamplesToAverage;
+    T* sampleArray;
+    uint16_t arrayIndex = 0;
+    SumType sum = 0;
+    T average = 0;
+
+
+public:
+    AverageFilter(uint16_t samplesToAverage)
+        : SamplesToAverage(samplesToAverage)
+    {
+        if (SamplesToAverage > 0)
+            sampleArray = new T[SamplesToAverage];
+        
+        reset();
+    }
+
+    ~AverageFilter()
+    {
+        if (SamplesToAverage > 0)
+            delete [] sampleArray;
+    }
+
+    T update(T newValue) override
+    {
+        // Move arrayIndex to the next position in array loop
+        arrayIndex++;
+        if (arrayIndex >= SamplesToAverage)
+            arrayIndex = 0;
+        
+        // Subtract oldest value form the sum and add the new one
+        sum -= sampleArray[arrayIndex];
+        sum += newValue;
+        sampleArray[arrayIndex] = newValue;
+
+        average = sum / (T)SamplesToAverage;
+        return average;
+    }
+
+    T getFilteredValue() override
+    {
+        return average;
+    }
+
+    void reset()
+    {
+        arrayIndex = 0;
+        sum = 0;
+        average = 0;
+
+        for (int i=0; i<SamplesToAverage; i++)
+            sampleArray[i] = 0;
+    }
+};
+
+
+#endif
